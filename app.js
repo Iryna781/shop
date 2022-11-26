c24edbebbb51ed06bf4ceb453fafe319ad98ecb5
Iryna <mulatka781@gmail.com>
2022-11-26 16:13:24 +0200

    add all

---
+++ app.js
@@ -0,0 +1,49 @@
+// let productsCountEl = document.getElementById("products-count");
+// console.log(productsCountEl);
+
+//add to cart
+// let addCartBtns = document.querySelectorAll(".btn-add");
+// console.log(addCartBtns);
+
+// for (let i = 0; i < addCartBtns.length; i++) {
+//   addCartBtns[i].addEventListener("click", function () {
+//     // let prevProductsCount = +productsCountEl.textContent;
+//     // productsCountEl.textContent = prevProductsCount + 1;
+//     productsCountEl.textContent = +productsCountEl.textContent + 1;
+//   });
+// }
+
+// //modal
+// let moreDetailsBtns = document.querySelectorAll(".btn-details");
+// let modal = document.querySelector(".modal");
+// let btnClose = document.querySelector(".btn-close");
+// console.log(btnClose);
+// // console.log(moreDetailsBtns);
+// // console.log(modal);
+
+// moreDetailsBtns.forEach((item) => {
+//   item.addEventListener("click", function () {
+//     modal.classList.add("show");
+//     modal.classList.remove("hide");
+//   });
+// });
+
+// btnClose.addEventListener("click", function () {
+//   modal.classList.add("hide");
+//   modal.classList.remove("show");
+// });
+
+// function openModal() {
+//   modal.classList.add("show");
+//   modal.classList.remove("hide");
+// }
+// function closeModal() {
+//   modal.classList.add("hide");
+//   modal.classList.remove("show");
+// }
+
+// modal.addEventListener("click", function (e) {
+//   if (e.target === modal) {
+//     closeModal();
+//   }
+// });
