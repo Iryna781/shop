4d4fc94562c2d727bef2c2ca88f3c8014b06292d
Iryna <mulatka781@gmail.com>
2022-11-28 17:35:29 +0200

    slider

--- app.js
+++ app.js
@@ -82,7 +82,8 @@ modal.addEventListener("click", function (e) {
 });
 
 //toggle like state
-let likeBtns = document.querySelectorAll(".like");
+let likeBtns = document.querySelectorAll(".heart");
+// console.log(likeBtns);
 likeBtns.forEach((item) => {
   item.addEventListener("click", function () {
     // if (item.classList.contains("liked")) {
@@ -91,21 +92,104 @@ likeBtns.forEach((item) => {
     //   item.classList.add("liked");
     // }
     item.classList.toggle("liked");
-  });
+      //  console.log("click");
+   });
 });
 
+// console.log(document.body.scrollHeight);
+
 function showModalByScroll() {
-  if (window.pageYOffset > document.body.scrollHeight / 2) {
+  if (window.scrollY > document.body.scrollHeight / 2) {
     openModal();
     window.removeEventListener("scroll", showModalByScroll);
   }
 }
 
 window.addEventListener("scroll", showModalByScroll);
+// setTimeout(openModal, 5000);
 
 //add slider
+// $(".slider-block").slick();
 
 $(".slider-block").slick({
   dots: true,
 });
 
+let decrementBtns = document.querySelectorAll(".decrement");
+let incrementBtns = document.querySelectorAll(".increment");
+let inputFields = document.querySelectorAll(".product-quantity input");
+
+console.log(decrementBtns);
+console.log(incrementBtns);
+console.log(inputFields);
+
+incrementBtns.forEach((item, i) => {
+  item.addEventListener("click", function(){
+   let currentCount = +inputFields[i].value;
+   inputFields[i].value = currentCount + 1;
+
+  });
+});
+
+decrementBtns.forEach((item, i) => {
+  item.addEventListener("click", function(){
+   let currentCount = +inputFields[i].value;
+   inputFields[i].value = currentCount - 1;
+
+  });
+});
+
+// let audi = {
+//   model:"A4",
+//   year: 2012,
+//   color:"black",
+// };
+// let bmw = {
+//   model:"328",
+//   year: 2010,
+//   color:"white",
+// };
+// let lexus = {
+//   model:"ES250",
+//   year: 2008,
+//   color:"grey",
+// };
+// console.log(audi);
+// console.log(bmw);
+// console.log(lexus);
+
+// function Car(model, year, color){
+//   this.model = model;
+//   this.year = year;
+//   this.color = color;
+//   this.showName = function(){
+//     return this.model;
+//   };
+// }
+// let audi = new Car("A4", 2012, "grey");
+// let lexus = new Car("ES250", 2020, "black");
+// let bmw = new Car("328", 2002, "white");
+// console.log(audi);
+// console.log(bmw);
+// console.log(lexus);
+// console.log(audi.showName());
+
+function Counter(incrementBtn, decrementBtn, inputField, minCount = 1, maxCount = 10){
+   this.domRefs={
+    incrementBtn,
+    decrementBtn,
+    inputField,
+   };
+  //  console.log(this);
+   this.toggleButtonState = function(){
+    let count = this.domRefs.inputField.value;
+    this.domRefs.decrementBtn.disabled = count <= minCount;
+    this.domRefs.incrementBtn.disabled = count >= maxCount;
+
+   };
+   this.toggleButtonState();
+
+   console.log(this);
+}
+
+let counter1 = new Counter(incrementBtns[0], decrementBtns[0],inputFields[0]);
